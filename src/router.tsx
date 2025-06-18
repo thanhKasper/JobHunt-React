854657a60c3e4fe9e782d645ed89a002e9c39cc6
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2025-06-18 17:27:29 +0700

    update: update routing with job filter page added

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,34 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "./layouts/RootLayout";
 import JobPage from "./pages/JobPage";
+import JobFilterPage from "./pages/JobFilter/JobFilterPage";
+import JobFilterPageDetail from "./pages/JobFilter/JobFilterPageDetail";
 
 let router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout/>,
+    element: <RootLayout />,
     children: [
       {
         index: true,
-        element: <JobPage/>
-      }
-    ]
-  }
+        element: <JobPage />,
+      },
+      {
+        path: "jobs",
+        element: <JobPage />,
+      },
+      {
+        path: "job-filters",
+        element: <JobFilterPage />,
+        children: [
+          {
+            path: "/job-filters/:jobFilterId",
+            element: <JobFilterPageDetail />,
+          },
+        ],
+      },
+    ],
+  },
 ]);
-
 
 export default router;
