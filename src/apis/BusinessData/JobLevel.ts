62f8447387e65a5c7b1de2e85f2301296bf5269b
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2025-06-19 11:30:48 +0700

    feat: add hard code data

---
+++ src/apis/BusinessData/JobLevel.ts
@@ -0,0 +1,36 @@
+const jobLevelList = [
+  {
+    key: "Intern",
+    value: "Thực Tập Sinh",
+  },
+  {
+    key: "Fresher",
+    value: "Nhân Viên Mới Ra Trường",
+  },
+  {
+    key: "Junior",
+    value: "Nhân Viên Mới",
+  },
+  {
+    key: "Middle",
+    value: "Nhân Viên Trung Cấp",
+  },
+  {
+    key: "Senior",
+    value: "Nhân Viên Cao Cấp",
+  },
+  {
+    key: "TeamLead",
+    value: "Trưởng Nhóm",
+  },
+  {
+    key: "Manager",
+    value: "Quản Lý",
+  },
+  {
+    key: "Director",
+    value: "Giám Đốc",
+  },
+];
+
+export default jobLevelList;
---
+++ src/apis/BusinessData/OccupationList.ts
@@ -0,0 +1,8 @@
+const ocupationList = [
+    {
+        key: "Information_Technology",
+        value: "Công Nghệ Thông Tin",
+    },
+]
+
+export default ocupationList;
