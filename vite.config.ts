16329ac315ea592a99cfe119eae0ff28aee9cdd4
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2025-06-19 11:27:49 +0700

    feat: set up alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), tailwindcss() 
-  ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@apis": path.resolve(__dirname, "./src/apis"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
